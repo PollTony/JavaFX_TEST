e05339da0718579d88d0c33572f429d83d831fb5
PollTony <tonipoll@seznam.cz>
2022-02-21 22:22:00 +0100

    Initial commit

---
+++ src/main/java/com/example/javafx_test/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.javafx_test;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX 5555555555 Application!");
+    }
+}
